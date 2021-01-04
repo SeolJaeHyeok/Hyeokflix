0350c50aa127f8684a1fdff7d9ed9c2903d1411e
seoljaehyeok <milkboy2564@naver.com>
2021-01-04 22:14:44 +0900

    React Router #2

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,9 +1,15 @@
 import React, { Component } from "react";
 import Router from "Components/Router";
+import Header from "Components/Header";
 
 class App extends Component {
   render() {
-    return <Router />;
+    return (
+      <>
+        <Header />
+        <Router />
+      </>
+    );
   }
 }
 
--- src/Components/Router.js
+++ src/Components/Router.js
@@ -1,13 +1,21 @@
 import React from "react";
-import { HashRouter as Router, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from "react-router-dom";
 import Home from "Routes/Home";
 import Search from "Routes/Search";
 import TV from "Routes/TV";
 
 export default () => (
   <Router>
-    <Route path="/" exact component={Home} />
-    <Route path="/search" exact component={Search} />
-    <Route path="/TV" exact component={TV} />
+    <Switch>
+      <Route path="/" exact component={Home} />
+      <Route path="/search" exact component={Search} />
+      <Route path="/tv" exact component={TV} />
+      <Redirect from="*" to="/" />
+    </Switch>
   </Router>
 );
