f767162a8c44c3a1a76b5da70fc49d582b1376cb
seoljaehyeok <milkboy2564@naver.com>
2021-01-07 15:37:17 +0900

    Header Styling

--- src/Components/GlobalStyles.js
+++ src/Components/GlobalStyles.js
@@ -12,8 +12,10 @@ const globalStyles = createGlobalStyle`
   }
   body {
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-    font-size: 14px;
+    font-size: 12px;
     background-color: rgba(20,20,20,1);
+    color: white;
+    padding-top: 50px;
   }
 `;
 
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,27 +2,45 @@ import React from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 
-const Header = styled.header``;
+const Header = styled.header`
+  color: white;
+  position: fixed;
+  top: 0px;
+  left: 0px;
+  width: 100%;
+  height: 50px;
+  display: flex;
+  align-items: center;
+  background-color: rgba(20, 20, 20, 0.8);
+  z-index: 10px;
+  box-shadow: 0px 1px 5px 2px rgba(0, 0, 0, 0.8);
+`;
 const List = styled.ul`
   display: flex;
-  &:hover {
-    background-color: blue;
-  }
 `;
-const Item = styled.li``;
-const SLink = styled(Link)``;
+const Item = styled.li`
+  width: 80px;
+  height: 50px;
+  text-align: center;
+`;
+const SLink = styled(Link)`
+  height: 50px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 export default () => (
   <Header>
     <List>
       <Item>
-        <SLink to="/">Home</SLink>
-      </Item>
-      <Item>
-        <SLink to="/search">Search</SLink>
+        <SLink to="/">Movies</SLink>
       </Item>
       <Item>
         <SLink to="/tv">TV</SLink>
+      </Item>
+      <Item>
+        <SLink to="/search">Search</SLink>
       </Item>
     </List>
   </Header>
