592a5cc7148463af4551d4a65d93a867213bac57
seoljaehyeok <milkboy2564@naver.com>
2021-01-08 13:40:33 +0900

    append_to_response Method를 통해 비디오와 이미지 등을 가져오기 위한 기본 설정

--- src/api.js
+++ src/api.js
@@ -12,14 +12,24 @@ export const moviesApi = {
   nowPlaying: () => api.get("movie/now_playing"),
   upcoming: () => api.get("movie/upcoming"),
   popular: () => api.get("movie/popular"),
-  movieDetail: (id) => api.get(`movie/${id}`),
+  movieDetail: (id) =>
+    api.get(`movie/${id}`, {
+      params: {
+        append_to_response: "videos",
+      },
+    }),
 };
 
 export const tvApi = {
   topRated: () => api.get("tv/top_rated"),
   popular: () => api.get("tv/popular"),
   airingToday: () => api.get("tv/airing_today"),
-  showDetail: (id) => api.get(`tv/${id}`),
+  showDetail: (id) =>
+    api.get(`tv/${id}`, {
+      params: {
+        append_to_response: "videos",
+      },
+    }),
 };
 
 export default api;
