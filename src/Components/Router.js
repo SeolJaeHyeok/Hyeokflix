452aea3d80c6549205c11a757512ed0c6db419ed
seoljaehyeok <milkboy2564@naver.com>
2021-01-11 15:10:07 +0900

    Movie와 TV의 Detail을 보여줄 Router 추가

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -8,6 +8,7 @@ import {
 import Home from "Routes/Home";
 import Search from "Routes/Search";
 import TV from "Routes/TV";
+import Detail from "Routes/Detail";
 import Header from "Components/Header";
 
 export default () => (
@@ -18,6 +19,8 @@ export default () => (
         <Route path="/" exact component={Home} />
         <Route path="/search" exact component={Search} />
         <Route path="/tv" exact component={TV} />
+        <Route path="/movie/:id" exact component={Detail} />
+        <Route path="/tv/:id" exact component={Detail} />
         <Redirect from="*" to="/" />
       </Switch>
     </>
