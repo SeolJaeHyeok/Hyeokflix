de7b423197c47bd3258bb4df17cd1707132de2eb
seoljaehyeok <milkboy2564@naver.com>
2021-01-11 13:45:41 +0900

    HomeContainer에서 ComponentDidMount가 발생했을때 API를 호출해 state를 업데이트 시킴

--- src/Routes/Home/HomeContainer.js
+++ src/Routes/Home/HomeContainer.js
@@ -1,3 +1,4 @@
+import { moviesApi } from "api";
 import React from "react";
 import HomePresenter from "./HomePresenter";
 
@@ -7,11 +8,38 @@ export default class extends React.Component {
     upComing: null,
     popular: null,
     error: null,
-    loading: null,
+    loading: true,
   };
 
+  async componentDidMount() {
+    try {
+      const {
+        data: { results: nowPlaying },
+      } = await moviesApi.nowPlaying();
+      const {
+        data: { results: upComing },
+      } = await moviesApi.upcoming();
+      const {
+        data: { results: popular },
+      } = await moviesApi.popular();
+      this.setState({
+        nowPlaying,
+        upComing,
+        popular,
+      });
+    } catch {
+      this.setState({
+        error: "Can't find the Moive Infomation.",
+      });
+    } finally {
+      this.setState({
+        loading: false,
+      });
+    }
+  }
   render() {
     const { nowPlaying, upComing, popular, error, loading } = this.state; // object destructing
+    console.log(this.state);
     return (
       <HomePresenter
         nowPlaying={nowPlaying}
