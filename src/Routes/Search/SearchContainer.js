332dafd05f2e81453f9faef04ab2ed8a904a6819
seoljaehyeok <milkboy2564@naver.com>
2021-01-15 16:00:06 +0900

    Search Presenter Styling 및 value 값 추적

--- src/Routes/Search/SearchContainer.js
+++ src/Routes/Search/SearchContainer.js
@@ -11,7 +11,8 @@ export default class extends React.Component {
     loading: false,
   };
 
-  handleSubmit = () => {
+  handleSubmit = (event) => {
+    event.preventDefault();
     const { searchTerm } = this.state;
     if (searchTerm !== "") {
       this.searchByTerm();
--- src/Routes/Search/SearchPresenter.js
+++ src/Routes/Search/SearchPresenter.js
@@ -2,6 +2,21 @@ import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
 
+const Container = styled.div`
+  padding: 0px 20px;
+`;
+
+const Form = styled.form`
+  margin-bottom: 50px;
+  width: 100%;
+`;
+
+const Input = styled.input`
+  all: unset;
+  width: 100%;
+  font-size: 28px;
+`;
+
 const SearchPresenter = ({
   movieResults,
   tvResults,
@@ -9,13 +24,22 @@ const SearchPresenter = ({
   error,
   loading,
   handleSubmit,
-}) => null;
+}) => (
+  <Container>
+    <Form onSubmit={handleSubmit}>
+      <Input
+        placeholder="Search Movie of TV Shows..."
+        value={searchTerm}
+      ></Input>
+    </Form>
+  </Container>
+);
 
-SearchPresenter.PropTypes = {
+SearchPresenter.propTypes = {
   movieResults: PropTypes.array,
   tvResults: PropTypes.array,
-  searchTerm: PropTypes.String,
-  error: PropTypes.String,
+  searchTerm: PropTypes.string,
+  error: PropTypes.string,
   loading: PropTypes.bool.isRequired,
   handleSubmit: PropTypes.func.isRequired,
 };
