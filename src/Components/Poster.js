a840a9604e37fd33cfca5e8385f400df88ff4874
seoljaehyeok <milkboy2564@naver.com>
2021-01-27 16:59:51 +0900

    Poster Styling 및 마무리

--- src/Components/Poster.js
+++ src/Components/Poster.js
@@ -3,15 +3,44 @@ import Proptypes from "prop-types";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 
-const Container = styled.div``;
+const Container = styled.div`
+  font-size: 12px;
+`;
 
-const ImageContainer = styled.div``;
+const Image = styled.div`
+  background-image: url(${(props) => props.bgUrl});
+  height: 180px;
+  background-size: cover;
+  background-position: center center;
+  border-radius: 5px;
+  transition: opacity 0.1s linear;
+`;
 
-const Image = styled.div``;
+const Rating = styled.span`
+  position: absolute;
+  bottom: 10px;
+  right: 5px;
+  opacity: 0;
+`;
 
-const Rating = styled.span``;
+const ImageContainer = styled.div`
+  margin-bottom: 5px;
+  position: relative;
+  &:hover {
+    ${Image} {
+      opacity: 0.3;
+    }
+    ${Rating} {
+      opacity: 1;
+    }
+  }
+  transition: opacity 0.1s linear;
+`;
 
-const Title = styled.span``;
+const Title = styled.span`
+  display: block;
+  margin-bottom: 3px;
+`;
 
 const Year = styled.span``;
 
@@ -19,7 +48,13 @@ const Poster = ({ id, imageUrl, title, rating, year, isMovie = false }) => (
   <Link to={isMovie ? `/movie/${id}` : `/tv/${id}`}>
     <Container>
       <ImageContainer>
-        <Image bgUrl={imageUrl} />
+        <Image
+          bgUrl={
+            imageUrl
+              ? `https://image.tmdb.org/t/p/w300${imageUrl}`
+              : "/assets/noPosterSmall.png"
+          }
+        />
         <Rating>
           <span role="img" aria-label="rating">
             ⭐️
@@ -27,7 +62,9 @@ const Poster = ({ id, imageUrl, title, rating, year, isMovie = false }) => (
           {rating} / 10
         </Rating>
       </ImageContainer>
-      <Title>{title}</Title>
+      <Title>
+        {title.length > 15 ? `${title.substring(0, 18)}...` : title}
+      </Title>
       <Year>{year}</Year>
     </Container>
   </Link>
