06872aae62d3f325ed17e6a770e7c552d5e17561
seoljaehyeok <milkboy2564@naver.com>
2021-01-29 14:13:26 +0900

    Detail Presenter - 컨텐츠 눌렀을 때 배경화면으로 포스터 띄우고 작은 크기에 포스터 하나더 화면에 띄우기 포스터가 없는 컨텐츠 처리 필요

--- src/Routes/Detail/DetailPresenter.js
+++ src/Routes/Detail/DetailPresenter.js
@@ -1,8 +1,62 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
+import Loader from "Components/Loader";
+// import noPosterSmall from "../assets/noPosterSmall.png";
 
-const DetailPresenter = ({ result, loading, error }) => null;
+const Container = styled.div`
+  height: calc(100vh - 50px);
+  width: 100%;
+  position: relative;
+  padding: 50px;
+`;
+
+const Backdrop = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-image: url(${(props) => props.bgImage});
+  background-position: center center;
+  background-size: cover;
+  filter: blur(3px);
+  opacity: 0.5;
+  z-index: 0;
+`;
+
+const Content = styled.div`
+  display: flex;
+  position: relative;
+  width: 100%;
+  height: 100%;
+  z-index: 0;
+`;
+
+const Cover = styled.div`
+  width: 30%;
+  background-image: url(${(props) => props.bgImage});
+  background-position: center center;
+  background-size: cover;
+  height: 100%;
+  border-radius: 5px;
+`;
+
+const DetailPresenter = ({ result, loading, error }) =>
+  loading ? (
+    <Loader />
+  ) : (
+    <Container>
+      <Backdrop
+        bgImage={`https://image.tmdb.org/t/p/original${result.backdrop_path}`}
+      />
+      <Content>
+        <Cover
+          bgImage={`https://image.tmdb.org/t/p/original${result.poster_path}`}
+        />
+      </Content>
+    </Container>
+  );
 
 DetailPresenter.propTypes = {
   result: PropTypes.object,
