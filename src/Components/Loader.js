593e4e15283ea2e4558d58d4e6ed744434deb81d
seoljaehyeok <milkboy2564@naver.com>
2021-02-16 18:30:36 +0900

    loading Img 바꿈 - yarn add로 패키지 다운

--- src/Components/Loader.js
+++ src/Components/Loader.js
@@ -1,27 +1,22 @@
 import React from "react";
-import styled from "styled-components";
+import ReactLoading from "react-loading";
 
-const Container = styled.div`
-  width: 100vw;
-  height: 100vh;
-  font-size: 32px;
-  display: flex;
-  justify-content: center;
-  margin-top: 50px;
-`;
-
-const LoadingImg = styled.img`
-  width: 100%;
-  height: calc(100% - 120px);
-  background-size: cover;
-  background-position: center center;
-`;
-
-export default () => (
-  <Container>
-    <LoadingImg
-      src="https://media1.giphy.com/media/3ohzdOrcdpiD26TPt6/giphy.gif?cid=ecf05e47crfi61b0bexys7bsu9ekn5uf7h79u4yfp8wmsjr1&rid=giphy.gif"
-      alt="Waiting"
-    ></LoadingImg>
-  </Container>
-);
+function Loader() {
+  return (
+    <div class="contentWrap">
+      {" "}
+      <div
+        style={{
+          position: "fixed",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+        }}
+      >
+        <h2>Waiting</h2>{" "}
+        <ReactLoading color={"white"} height={"80%"} width={"80%"} />{" "}
+      </div>{" "}
+    </div>
+  );
+}
+export default Loader;
