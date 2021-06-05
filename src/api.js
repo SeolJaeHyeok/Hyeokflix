dcff870a7f60d7f1ecd338321f77aadf20d3752b
Jaehyeok <milkboy2564@naver.com>
2021-06-06 01:56:02 +0900

    API 호출할 때 언어 한국어로 변경

--- src/api.js
+++ src/api.js
@@ -4,7 +4,7 @@ const api = axios.create({
   baseURL: "https://api.themoviedb.org/3/",
   params: {
     api_key: "c44bcd2c30bdd8361a44243773d49b6f",
-    language: "en-US",
+    language: "ko",
   },
 });
 
