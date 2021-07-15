10946e55506c3faeba9e33108fae9f8aafcf8696
Jaehyeok <milkboy2564@naver.com>
2021-07-15 14:30:10 +0900

    v2 - Router Setting
    Redirection 기능 및 Switch를 사용해서 한 번에 하나의 Component를 render할 수 있도록 설정

---
+++ src/components/Router.js
@@ -0,0 +1,26 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Redirect,
+  Route,
+  Switch,
+} from "react-router-dom";
+
+import Home from "pages/Home";
+import Tv from "pages/Tv";
+import Detail from "pages/Detail";
+import Search from "pages/Search";
+
+export default () => (
+  <>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/tv" component={Tv} />
+        <Route path="/search" component={Search} />
+        <Route path="/detail" component={Detail} />
+        <Redirect from="*" to="/" />
+      </Switch>
+    </Router>
+  </>
+);
