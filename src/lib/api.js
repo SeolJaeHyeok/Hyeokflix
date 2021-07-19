5f93def25137454706051008d9de978615fcef50
Jaehyeok <milkboy2564@naver.com>
2021-07-19 21:48:06 +0900

    v2 - API Verbs
    기존에 있던 API + 이번에 추가한 API까지

---
+++ src/lib/api.js
@@ -0,0 +1,58 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "https://api.themoviedb.org/3/",
+  params: {
+    api_key: "c44bcd2c30bdd8361a44243773d49b6f",
+    language: "en-US",
+  },
+});
+
+export const movieApi = {
+  nowPlaying: () => api.get("movie/now_playing"),
+  upcoming: () => api.get("movie/upcoming"),
+  popular: () => api.get("movie/popular"),
+  trending: () => api.get(`trending/movie/week`),
+  similarMovie: (movie_id) => api.get(`movie/${movie_id}/similar`),
+  credits: (movie_id) => api.get(`movie/${movie_id}/credits`),
+  actorInformation: (credit_id) => api.get(`credit/${credit_id}`),
+  movieDetail: (movie_id) =>
+    api.get(`movie/${movie_id}`, {
+      params: {
+        append_to_response: "videos",
+      },
+    }),
+  search: (term) =>
+    api.get("search/movie", {
+      params: {
+        query: encodeURIComponent(term), //특수문자나 스페이스 등을 인코딩 하는 과정이 꼭 필요!,
+      },
+    }),
+};
+
+export const tvApi = {
+  topRated: () => api.get("tv/top_rated"),
+  popular: () => api.get("tv/popular"),
+  airingToday: () => api.get("tv/airing_today"),
+  trending: () => api.get(`/trending/tv/week`),
+  similarTv: (tv_id) => api.get(`movie/${tv_id}/similar`),
+  credits: (tv_id) => api.get(`tv/${tv_id}/credits`),
+  actorInformation: (credit_id) => api.get(`credit/${credit_id}`),
+  showDetail: (tv_id) =>
+    api.get(`tv/${tv_id}`, {
+      params: {
+        append_to_response: "videos",
+      },
+    }),
+  search: (term) =>
+    api.get("search/tv", {
+      params: {
+        query: encodeURIComponent(term), // params를 query를 요구했는데 term을 정의해줘서 에러 발생!
+      },
+    }),
+};
+
+// api.get("movie/508943/similar");
+// api.get("movie/508943/reviews");
+
+export default api;
