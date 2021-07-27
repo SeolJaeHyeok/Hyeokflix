c83428f28f5d017724aa1c2e32951d5fc4dce94e
Jaehyeok <milkboy2564@naver.com>
2021-07-27 14:13:13 +0900

    v2 - Header Component Styling
    Search 대신 돋보기 아이콘 사용

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,6 +1,10 @@
 import React from "react";
 import { Link, withRouter } from "react-router-dom";
 import styled from "styled-components";
+import { search } from "react-icons-kit/icomoon/search";
+import { withBaseIcon } from "react-icons-kit";
+
+const IconBlock = withBaseIcon({ size: 24, style: { color: "White" } });
 
 const HeaderBlock = styled.header`
   color: white;
@@ -14,19 +18,32 @@ const HeaderBlock = styled.header`
   background-color: rgba(20, 20, 20, 0.8);
   z-index: 10;
   box-shadow: 0px 1px 5px 2px rgba(0, 0, 0, 0.8);
+  padding: 0px 10px;
 `;
 
 const HeaderList = styled.ul`
+  margin-top: 10px;
   display: flex;
+  float: left;
 `;
 
 const HeaderItem = styled.li`
+  font-size: 16px;
   width: 80px;
   height: 50px;
   text-align: center;
   border-bottom: 3px solid
     ${(props) => (props.current ? "#eb2f06" : "transparent")};
   transition: ease-in-out 0.2s border-bottom;
+`;
+
+const RightHeader = styled.div`
+  position: fixed;
+  right: 0;
+  padding-right: 25px;
+  float: right;
+  display: flex;
+  font-size: 20px;
 `;
 
 const SLink = styled(Link)`
@@ -57,9 +74,11 @@ const Header = ({ location: { pathname } }) => (
       <HeaderItem current={pathname === "/tv"}>
         <SLink to="/tv">TV</SLink>
       </HeaderItem>
-      <HeaderItem current={pathname === "/search"}>
-        <SLink to="/search">Search</SLink>
-      </HeaderItem>
+      <RightHeader>
+        <SLink to="/search">
+          <IconBlock icon={search} />
+        </SLink>
+      </RightHeader>
     </HeaderList>
   </HeaderBlock>
 );
