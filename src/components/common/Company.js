8fa357af8fc64cea548a1744c4d9369a87fa9744
Jaehyeok <milkboy2564@naver.com>
2021-07-30 14:05:28 +0900

    v2 - Company Component
    Production Company 정보를 나타내는 Component

---
+++ src/components/common/Company.js
@@ -0,0 +1,84 @@
+import React from "react";
+import styled from "styled-components";
+
+/*
+result.production_companies
+ */
+
+const Header = styled.div`
+  margin-top: 50px;
+  font-size: 24px;
+  color: white;
+`;
+
+const CompaniesBlock = styled.div`
+  width: 100%;
+  margin: 30px auto;
+  display: grid;
+  grid-gap: 10px;
+  grid-template-columns: repeat(auto-fill, 120px);
+  grid-auto-rows: 200px;
+`;
+
+const Name = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translateX(-50%) translateY(-50%);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: white;
+  font-size: 10px;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.7);
+  opacity: 0;
+  transition: 0.3s ease-in-out;
+`;
+
+const CompaniesItem = styled.div`
+  background: ${(props) => `url(${props.bgUrl})`};
+  background-size: cover;
+  background-position: center center;
+  background-repeat: no-repeat;
+  position: relative;
+  border-radius: 10px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: white;
+  &:hover {
+    ${Name} {
+      opacity: 1;
+    }
+  }
+`;
+
+const Company = ({ company }) => {
+  return (
+    <>
+      {company && (
+        <>
+          <Header>Production Company</Header>
+          <CompaniesBlock>
+            {company.map((com) => (
+              <CompaniesItem
+                key={company.id}
+                bgUrl={
+                  company.logo_path
+                    ? `https://image.tmdb.org/t/p/w300${company.logo_path}`
+                    : "https://kknd26.ru/images/no-photo-nevinka.png"
+                }
+              >
+                <Name>{company.name}</Name>
+              </CompaniesItem>
+            ))}
+          </CompaniesBlock>
+        </>
+      )}
+    </>
+  );
+};
+
+export default Company;
